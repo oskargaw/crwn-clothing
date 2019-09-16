54bbcdd7c1fe8b0628236e0adc0b2831a294288b
Oskar Gawlak <oskar.gawlak@gmail.com>
2019-09-16 12:57:57 -0400

    Changed shop data to be object structured, created new selector for collections overview and updated our components with the new selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,11 +3,22 @@ import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
-  return <div className="collection-page">COLLECTION PAGE</div>;
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 // mapStateToProps gets to arguments, "state" and "own props" which are the props of the component we get
